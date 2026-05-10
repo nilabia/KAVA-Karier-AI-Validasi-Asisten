69cc6966ae81640e2eebe72248fdeec33b19e3be
nilabia <nilabiidznillah@gmail.com>
2026-05-11 00:27:35 +0700

    feat: add landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,17 @@ import './index.css';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';  
 import AuthPage from './pages/AuthPage';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import LandingPage from './pages/LandingPage';
+import DashboardPage from './pages/DashboardPage';
 
 function App() {
   return (
     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Navigate to="auth" />} />
+          <Route path="/" element={<LandingPage />} />
           <Route path="/auth" element={<AuthPage />} />
+          <Route path="/dashboard" element={<DashboardPage/>}/>
         </Routes>
       </BrowserRouter>
     </GoogleOAuthProvider>
